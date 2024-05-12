e60119bb327c401933a876fffefaf5a608a54dbb
Hansana <it22563200@my.sliit.lk>
2024-05-12 21:29:13 +0530

    first commit

---
+++ app/src/main/java/com/example/noteapp/NotesAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.noteapp
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class NotesAdapter(private var notes:List<Task>, context: Context) : RecyclerView.Adapter<NotesAdapter.NoteViewHolder>(){
+
+            class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+                val titleTextView : TextView = itemView.findViewById(R.id.titleTextView)
+                val contentTextView :TextView = itemView.findViewById(R.id.contentTextView)
+            }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
